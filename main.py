54ca66eeb0e08631af69fe84a1dae4da37910d5e
Ken980 <kenboy9800@gmail.com>
2020-11-15 08:26:12 +0700

    add get latest skyblock news
    get latest skyblock news and update get player online

--- main.py
+++ main.py
@@ -21,24 +21,10 @@ def api(api, bool):
     print(response.status_code)
     print(api)
 
-'''
-name = ""
-uuid = ""
-page = ""
-
-api_key = "http://api.hypixel.net/key?key=" + key
-api_name = "http://api.hypixel.net/player?key="+ key + "&name=" + name
-api_uuid = "http://api.hypixel.net/player?key="+ key + "&uuid=" + uuid
-api_friends = "http://api.hypixel.net/friends?key="+ key + "&uuid=" + uuid
-api_auction_total = "http://api.hypixel.net/skyblock/auctions?key="+ key + "&page=" + page
-api_auction_player = "http://api.hypixel.net/skyblock/auction?key="+ key + "&uuid=" + uuid
-api_gamecounts = "http://api.hypixel.net/gameCounts?key="+ key
-'''
-
 key = input("API Key: ")
 
 while True:
-    api_function = input("Choose function:\n0) Quit\n1) Get key data\n2) Get player data from username\n3) Get player from UUID\n4) Get friends UUID\n5) Get auction item data\n6) Get player auction data\n7) Get game count data\n8) Get player last seen\n9) Get player count online\n")
+    api_function = input("Choose function:\n0) Quit\n1) Get key data\n2) Get player data from username\n3) Get player from UUID\n4) Get friends UUID\n5) Get auction item data\n6) Get player auction data\n7) Get game count data\n8) Get player last seen\n9) Get player count online\n10) Get latest skyblock news\n")
     if api_function == "1":
         key_api_function = input("API Key: ")
         api_key = "http://api.hypixel.net/key?key=" + key_api_function
@@ -102,7 +88,16 @@ while True:
             print("Error")
     elif api_function == "9":
         api_player_count = "http://api.hypixel.net/playerCount?key=" + key
-        api(api_player_count, True)
+        response = requests.get(api_player_count)
+        print("Player: " + response.json()["playerCount"])
+    elif api_function == "10":
+        api_news = "http://api.hypixel.net/skyblock/news?key=" + key
+
+        response = requests.get(api_news)
+
+        print(response.json()["items"][0]["title"])
+        print(response.json()["items"][0]["text"])
+        print(response.json()["items"][0]["link"])
     elif api_function == "0":
         break
     else:
