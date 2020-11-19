cf21baf79950c4938ed70997fc28731793843013
Ken980 <kenboy9800@gmail.com>
2020-11-19 10:01:24 +0700

    add input
    search player input

--- stats viewer/main.py
+++ stats viewer/main.py
@@ -1,21 +1,40 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 import requests
 import json
+import os
+import time
 
 app = Flask(__name__)
 
-key = ""
+key = "17104bc5-2957-4562-a50f-a2b28f5c0828"
 url = "http://api.hypixel.net/player?key="
 
-@app.route("/")
-def home(name):
-    return render_template("index.html")
+def format_server_time():
+    server_time = time.localtime()
+    return time.strftime("%I:%M:%S %p", server_time)
 
-@app.route("/player/stats/<username>")
+@app.route("/", methods = ["POST", "GET"])
+def home():
+    context = {"server_time": format_server_time()}
+    if request.method == "POST":
+        username = request.form["username"]
+        return redirect(url_for("stats", username=username))
+    else:
+        return render_template("index.html", context=context)
+
+@app.route("/player/stats/<username>", methods = ["POST", "GET"])
 def stats(username):
-    response = requests.get(url + key + "&name=" + username)
-    DisplayName = response.json()["player"]["displayname"]
-    return render_template("index.html", username=DisplayName)
+    context = {"server_time": format_server_time()}
+    if request.method == "POST":
+        username = request.form["username"]
+        return redirect(url_for("stats", username=username))
+    elif username != None:
+        response = requests.get(url + key + "&name=" + username)
+        player = {"displayname": response.json()["player"]["displayname"]}
+        return render_template("stats.html", username=player, context=context)
+    else:
+        return redirect(url_for("home"))
 
 if __name__ == "__main__":
-    app.run()
+#    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True)
